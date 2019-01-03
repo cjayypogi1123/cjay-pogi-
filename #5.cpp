74b0ef800e3cfd5e971dcf3cb01e11487d91967d
cjayypogi1123 <46342339+cjayypogi1123@users.noreply.github.com>
2019-01-03 23:21:40 +0800

    Add files via upload

---
+++ #5.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+void linechar() 
+{
+	 for(int a = 1; a <= 20; a++) 
+	 { 
+        cout<<"*";
+     }
+    	cout<<"\n";
+    	cout<<"\n";
+     
+     for(int b = 1; b <= 20; b++) 
+	 { 
+        cout<<"@";
+     }
+     	cout<<"\n";
+     	cout<<"\n";
+     	cout<<"\n";
+    
+     for(int x = 1; x <= 10; x++) 
+	 { 
+        cout<<"*";
+     }
+     	cout<<"\n";
+     	cout<<"\n";
+     
+     for(int y = 1; y <= 15; y++) 
+	 { 
+        cout<<"#";
+     }
+     	cout<<"\n";
+     	cout<<"\n";
+ 	}
+ 	
+int main()
+ 	{
+    linechar();   
+    return 0;
+ }
