d63a77aad8af5244c827bff865f0d1a7c8a7b190
cjayypogi1123 <46342339+cjayypogi1123@users.noreply.github.com>
2019-01-03 23:16:24 +0800

    Update #5.cpp

--- #5.cpp
+++ #5.cpp
@@ -1,31 +1,39 @@
 #include <iostream>
- using namespace std;
- void linechar(char) 
- {
-     for(int i = 1; i <= 20; i++) { 
-         cout << "*";
+using namespace std;
+void linechar() 
+{
+	 for(int i = 1; i <= 20; i++) 
+	 { 
+        cout<<"*";
      }
-     cout<<endl;
-     cout<<endl;
-     for(int b = 1; b <= 20; b++) { 
-         cout << "@";
-     }
-     cout<<endl;
-     cout<<endl;
+    	cout<<"\n";
+    	cout<<"\n";
      
-     for(int a = 1; a <= 10; a++) { 
-         cout << "*";
+     for(int b = 1; b <= 20; b++) 
+	 { 
+        cout<<"@";
      }
-     cout<<endl;
-     cout<<endl;
-     for(int a = 1; a <= 15; a++) { 
-         cout << "#";
+     	cout<<"\n";
+     	cout<<"\n";
+     	cout<<"\n";
+    
+     for(int a = 1; a <= 10; a++) 
+	 { 
+        cout<<"*";
      }
-     cout<<endl;
-     cout<<endl;
- }
- int main()
- {
-    void linechar(char);   
-     return 0;
+     	cout<<"\n";
+     	cout<<"\n";
+     
+     for(int a = 1; a <= 15; a++) 
+	 { 
+        cout<<"#";
+     }
+     	cout<<"\n";
+     	cout<<"\n";
+ 	}
+ 	
+int main()
+ 	{
+    linechar();   
+    return 0;
  }
